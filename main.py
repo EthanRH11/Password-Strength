5380c545d13316394bd873c30495e22a475024f3
EthanHicks <ehicks12@kent.edu>
2024-12-12 11:56:36 -0500

    Inital commit with to do list/ outline of functionality

---
+++ main.py
@@ -0,0 +1,6 @@
+#Create a function to evaluate the password, length, capital letters, and atleast 2 special characters
+
+#Create a function that returns reccomendations to make the password stronger
+
+#Shows password being used again John The Ripper? Be able to show the user
+#that there password is either easy to be cracked or hard to crack
